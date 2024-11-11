64c4fee8178c2307a8f7727dda909a49dff8b6b6
MaksDeb <maks.debek@gmail.com>
2024-11-11 12:34:54 +0100

    Added training and evaluation pipeline for random forest with cross validation, todo: -create master pipeline -rename all the files properly -optimize neural network for better results

--- pipelines/masterpipeline.py
+++ pipelines/masterpipeline.py
@@ -11,6 +11,6 @@ def masterpipeline():
         config_path='dataengineeringconfig.yml')()
     splittingdatapipeline_instance = splittinddatapipeline.splittingdatapipeline.with_options(
         config_path='splittingdataconfig.yml')()
-    trainandpredictpipeline_instace = trainandpredictpipelineneuralnetwork.train_neuralnetwork_pipeline()
+    trainandpredictpipeline_instace = trainpipelineneuralnetwork.train_neuralnetwork_pipeline()
     modelevaluationpipeline_instance = neuralnetwork_modelevaluationpipeline.neuralnetwork_modelevaluationpipeline()
 
